2a05a6f45dad5effbe860fcadeb3d14697c57470
fanGam <kosyurakosyura@gmail.com>
2023-10-20 13:34:02 +0500

    Добавьте файлы проекта.

---
+++ Money.cs
@@ -0,0 +1,195 @@
+﻿using System.ComponentModel;
+using System.Data.Common;
+
+namespace Lab2CH
+{
+    public class Money
+    {
+        //Закрытые поля нужных типов
+        private uint rubles;
+        private byte kopeiki;
+        //Свойства которые дают только чтение
+        public uint ruble
+        {
+            get { return rubles; }
+        }
+
+        public byte kopeika
+        {
+            get { return kopeiki; }
+        }
+        //Базовый конструктор
+        public Money()
+        {
+            rubles = 0;
+            kopeiki = 0;
+        }
+        //Конструктор копии
+        public Money(Money obj)
+        {
+            this.rubles = obj.ruble;
+            this.kopeiki = obj.kopeika;
+        }
+        //Конструктор если пользователь ввёл рубли и копейки отдельно
+        public Money(int a, int b) : this()
+        {
+            rubles = (uint)Math.Abs(a + (b / 100));
+            kopeiki = (byte)(Math.Abs(b) % 100);
+        }
+        //Конструктор если пользователь ввёл числом типа double
+        public Money(double a) : this()
+        {
+            rubles = (uint)Math.Abs(a);
+            kopeiki = (byte)(Math.Abs(a * 100) % 100);
+        }
+        //Конструктор если пользователь ввёл текстом (основной)
+        public Money(string a) : this()
+        {
+            if (a == "")
+            {
+                kopeiki = 0;
+                rubles = 0;
+            }
+            else
+            {
+                a = a.Replace('.', ',');
+                try
+                {
+                    int b;
+                    b = (int)(double.Parse(a) * 100);
+                    if (kopeiki + b < 0)
+                    {
+                        kopeiki = 0;
+                        rubles = 0;
+                    }
+                    else
+                    {
+                        rubles = (uint)(b) / 100;
+                        kopeiki = (byte)(b % 100);
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    ConsoleColor tmp1 = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\nКажется кто-то не умеет писать циферки :(");
+                    Console.ForegroundColor = tmp1;
+                }
+            }
+        }
+        //Фукция которая возвращает баланс кошелька в типе double
+        public double GetAmount()
+        {
+            return (rubles + (double)kopeiki / 100);
+        }
+        //Метод изменения цены при входных типа <string>
+        public void PriceChange(string a)
+        {
+            if (a != "")
+            {
+                a = a.Replace('.', ',');
+                try
+                {
+                    int b, c;
+                    b = (int)(double.Parse(a) * 100);
+                    c = (int)(rubles * 100 + kopeiki);
+                    if (kopeiki + b < 0)
+                    {
+                        kopeiki = 0;
+                        rubles = 0;
+                    }
+                    else
+                    {
+                        rubles = (uint)(kopeiki + b) / 100;
+                        kopeiki = (byte)((c + b) % 100);
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    ConsoleColor tmp1 = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\nКажется кто-то не умеет писать циферки :(");
+                    Console.ForegroundColor = tmp1;
+                    rubles = 0;
+                    kopeiki = 0;
+                }
+            }
+        }
+        //Метод изменения цены при входных типа <double>
+        public void PriceChange(double a)
+        {
+            double b = rubles + (double)kopeiki / 100;
+            if (b + a < 0)
+            {
+                kopeiki = 0;
+                rubles = 0;
+            }
+            else
+            {
+                rubles = (uint)(b + a);
+                kopeiki = (byte)(((b + a) * 100) % 100);
+            }
+        }
+        //Переопределение операторов ++, --, +, -, >, <, >=, <=, ==, !=.
+        public static Money operator ++(Money obj)
+        {
+            obj.PriceChange(0.01);
+            return obj;
+        }
+        public static Money operator --(Money obj)
+        {
+            obj.PriceChange(-0.01);
+            return obj;
+        }
+        public static Money operator +(Money obj1, Money obj2)
+        {
+            Money obj3 = new Money(obj1);
+            obj3.PriceChange(obj2.GetAmount().ToString());
+            return obj3;
+        }
+        public static Money operator -(Money obj1, Money obj2)
+        {
+            Money obj3 = new Money(obj1);
+            obj3.PriceChange(obj2.GetAmount().ToString());
+            return obj3;
+        }
+        public static bool operator <(Money obj1, Money obj2)
+        {
+            return (obj1.GetAmount() < obj2.GetAmount());
+        }
+        public static bool operator >(Money obj1, Money obj2)
+        {
+            return (obj1.GetAmount() > obj2.GetAmount());
+        }
+        public static bool operator <=(Money obj1, Money obj2)
+        {
+            return (obj1.GetAmount() <= obj2.GetAmount());
+        }
+        public static bool operator >=(Money obj1, Money obj2)
+        {
+            return (obj1.GetAmount() >= obj2.GetAmount());
+        }
+        public static bool operator ==(Money obj1, Money obj2)
+        {
+            return (obj1.GetAmount() == obj2.GetAmount());
+        }
+        public static bool operator !=(Money obj1, Money obj2)
+        {
+            return (obj1.GetAmount() != obj2.GetAmount());
+        }
+        //Операции приведения типа uint для копеек и bool для баланса
+        public static explicit operator uint(Money obj)
+        {
+            return obj.kopeika;
+        }
+        public static implicit operator bool(Money obj)
+        {
+            return obj.GetAmount() != 0;
+        }
+        //Переопределение метода ToString()
+        public override string ToString()
+        {
+            return $"{rubles} рублей и {kopeiki} копеек";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿namespace Lab2CH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Основной кусок истории
+            Console.WriteLine("Вот Гоша сходил с друзьями в кино и они пошли гулять.\n" +
+                "Пока они гуляли решили зайти в магазин за продуктами (и не только),\n" +
+                "но есть проблема, Паше сложно даётся математика и ему нужна помощь посчитать.\n" +
+                "В этот раз будем считать денюжки!");
+            Console.WriteLine("Введите бюджет Гоши!");
+            Money Gosha;
+            Gosha = new Money(Console.ReadLine());
+            Console.WriteLine($"Итого у Гоши есть {Gosha}\n" +
+                $"Ему нужно купить немного вкусняшек чтобы угостить его друзей\n" +
+                $"Вот список того что ему нужно купить:\n" +
+                $"1) Колу\n" +
+                $"2) Чипсы\n" +
+                $"3) Шоколадку\n" +
+                $"Напишите ценник продуктов!");
+
+            Console.WriteLine("----------------------------------");
+
+            //Покупка колы в магазине
+            Console.WriteLine($"Цена колы:");
+            Money Cola = new Money(Console.ReadLine());
+            if (Gosha >= Cola)
+            {
+                if (Cola.GetAmount() == 0)
+                {
+                    Console.WriteLine("Видимо в магазине акция и кола бесплатная!");
+                }
+            }
+            if (Gosha > Cola)
+            {
+                Gosha.PriceChange((-Cola.GetAmount()));
+                Console.WriteLine($"Гоша купил колу себе и друзьям!" +
+                    $"У него осталось!");
+                Console.WriteLine($"{Gosha}");
+            }
+            else
+            {
+                Console.WriteLine("У Гоши не хватает денег для покупки Колы! :(");
+            }
+
+            //Находка копейки на полу
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine("Пока Гоша шёл за Чипсами он нашёл копеечку на полу и забрал её себе!");
+            Console.WriteLine($"Теперь у него {++Gosha}");
+
+            Console.WriteLine("----------------------------------");
+
+            //Покупка чипсов
+            Console.WriteLine("Цена чипсов:");
+            Money Chips = new Money(Console.ReadLine());
+            if (Gosha >= Chips)
+            {
+                if (Chips.GetAmount() == 0)
+                {
+                    Console.WriteLine("Видимо в магазине акция и чипсы бесплатные!");
+                }
+            }
+            if (Gosha > Chips)
+            {
+                Gosha.PriceChange((-Chips.GetAmount()));
+                Console.WriteLine($"Гоша купил чипсы себе и друзьям!\n" +
+                    $"У него осталось!");
+                Console.WriteLine($"{Gosha}");
+            }
+            else
+            {
+                Console.WriteLine("У Гоши не хватает денег для покупки Чипсов! :(");
+            }
+
+            Console.WriteLine("----------------------------------");
+
+            //Помощь Гоше если у него нет денег
+            if (Gosha.GetAmount() > 0.01)
+            {
+                Console.WriteLine("Пока Гоша шёл за шоколадкой он обронил найденную монетку\n" +
+                    "Вот такой круговорот монет в магазине");
+                Console.WriteLine($"Теперь у него {--Gosha}");
+            }
+            else
+            {
+                Console.WriteLine("Так как у Гоши не осталось денег то Паша решил ему помочь\n" +
+                    "Сколько денег Паша дал Гоше");
+                Money Pasha = new Money(Console.ReadLine());
+                Gosha = Gosha + Pasha;
+                Console.WriteLine($"Теперь у Гоши {Gosha}");
+            }
+
+            Console.WriteLine("----------------------------------");
+
+            //Покупка шоколадки
+            Console.WriteLine("Цена шоколадки:");
+            Money Chokolate = new Money(Console.ReadLine());
+            if (Gosha >= Chokolate)
+            {
+                if (Chokolate.GetAmount() == 0)
+                {
+                    Console.WriteLine("Видимо в магазине акция и шоколадка бесплатная!");
+                }
+            }
+            if (Gosha >= Chokolate)
+            {
+                Gosha.PriceChange((-Chokolate.GetAmount()));
+                Console.WriteLine($"Гоша купил шоколадку себе и друзьям!\n" +
+                    $"У него осталось!");
+                Console.WriteLine($"{Gosha}");
+            }
+            else
+            {
+                Console.WriteLine("У Гоши не хватает денег для покупки шоколадки! :(");
+            }
+
+            Console.WriteLine("----------------------------------");
+
+            //Результат похода
+            if (Gosha)
+            {
+                Console.WriteLine("После всей траты у Гоши ещё остались денюжки!");
+                Console.WriteLine($"{Gosha}");
+            }
+            else
+            {
+                Console.WriteLine("Ну всё, Гоша остался без денег...");
+                Console.WriteLine($"{Gosha}");
+            }
+        }
+    }
+}
